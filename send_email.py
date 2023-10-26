91ba5fb80dda700e6a07efb338f7992ae150ad94
Amine Hifdi <101675260+OVK-CyberSec@users.noreply.github.com>
2023-10-26 21:26:07 +0100

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,23 +1,26 @@
-import smtplib, ssl
+import smtplib
+import ssl
 
+# Define a function to send an email with a given message
 def sendEmail(message):
-	smtp_server = "smtp.gmail.com"
-	port = 587 
-	sender_email = "your - email - here"
-	password = "enter your password"
-	receiver_email = "your - email - here"
+    smtp_server = "smtp.gmail.com"  # SMTP server for Gmail
+    port = 587  # Port for TLS (Transport Layer Security) connection
+    sender_email = "your-email-here"  # Replace with the sender's email address
+    password = "enter-your-password"  # Replace with the sender's email password
+    receiver_email = "your-email-here"  # Replace with the recipient's email address
 
-	context = ssl.create_default_context()
+    context = ssl.create_default_context()  # Create a secure SSL/TLS context
 
-	try:
-	    server = smtplib.SMTP(smtp_server,port)
-	    server.ehlo() 
-	    server.starttls(context=context) 
-	    server.ehlo()
-	    server.login(sender_email, password)
-	    server.sendmail(sender_email, receiver_email, message)
-	   
-	except Exception as e:
-	    print(e)
-	finally:
-	    server.quit()
+    try:
+        server = smtplib.SMTP(smtp_server, port)  # Create an SMTP server connection
+        server.ehlo()  # Identify yourself to the SMTP server
+        server.starttls(context=context)  # Upgrade the connection to use TLS
+        server.ehlo()  # Reidentify after the TLS handshake
+        server.login(sender_email, password)  # Login to the sender's email account
+        server.sendmail(sender_email, receiver_email, message)  # Send the email
+
+    except Exception as e:
+        print(e)  # Print any exceptions or errors that occur during the process
+
+    finally:
+        server.quit()  # Ensure that the SMTP server connection is properly closed
